daf1ae4c0db9eb58d0f058050282334548089792
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-05-02 13:47:48 -0300

    Rotas configuradas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
